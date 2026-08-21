852d29405a4e01151b0625446f2c1c8d8b0b756b
Heshtin <nitheshkrishnan.kn@gmail.com>
2026-08-21 14:52:46 +0800

    Level 0. Rename main class to Augustus

---
+++ src/main/java/Augustus.java
@@ -0,0 +1,12 @@
+public class Augustus {
+    public static void main(String[] args) {
+        String border = "__________________________";
+        System.out.println(border);
+        System.out.println("Hello! I'm Augustus");
+        System.out.println("What can I do for you?");
+        System.out.println(border);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(border);
+
+    }
+}
