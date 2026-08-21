c57bd0a8ef907b41dc87bb3ba88d99ed33d46ca5
Heshtin <nitheshkrishnan.kn@gmail.com>
2026-08-21 15:34:09 +0800

    Level 2. Add task, list

--- src/main/java/Augustus.java
+++ src/main/java/Augustus.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Augustus {
     public static void main(String[] args) {
@@ -8,20 +9,31 @@ public class Augustus {
         System.out.println(border);
 
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
 
-        while(true){
+        while(true) {
             String input = sc.nextLine();
-            if(input.equals("bye")) {
+
+            if (input.equals("bye")) {
                 System.out.println(border);
                 System.out.println("Bye. Thank you for using this chatbot");
                 System.out.println(border);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(border);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(border);
             }
-            System.out.println(border);
-            System.out.println(input);
-            System.out.println(border);
-
+            else {
+                tasks.add(input);
+                System.out.println(border);
+                System.out.println("added: " +input);
+                System.out.println(border);
+            }
         }
+        sc.close();
 
         System.out.println("Hope to see you again soon!");
         System.out.println(border);
