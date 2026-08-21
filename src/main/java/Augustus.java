146b8067a2b27ec87d97a7a88866355c96c99636
Heshtin <nitheshkrishnan.kn@gmail.com>
2026-08-21 15:18:15 +0800

    Level 1. Add echo support

--- src/main/java/Augustus.java
+++ src/main/java/Augustus.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Augustus {
     public static void main(String[] args) {
         String border = "__________________________";
@@ -5,7 +6,24 @@ public class Augustus {
         System.out.println("Hello! I'm Augustus");
         System.out.println("What can I do for you?");
         System.out.println(border);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            String input = sc.nextLine();
+            if(input.equals("bye")) {
+                System.out.println(border);
+                System.out.println("Bye. Thank you for using this chatbot");
+                System.out.println(border);
+                break;
+            }
+            System.out.println(border);
+            System.out.println(input);
+            System.out.println(border);
+
+        }
+
+        System.out.println("Hope to see you again soon!");
         System.out.println(border);
 
     }
